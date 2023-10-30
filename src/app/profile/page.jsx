f47e50a069a51703d4b290c7cbe816647e829c99
YsisC <ysisclongart@gmail.com>
2023-10-30 11:12:43 -0300

    Start dashboard medic

---
+++ src/app/profile/page.jsx
@@ -0,0 +1,19 @@
+
+import Image from "next/image";
+
+import { Metadata } from "next";
+export const metadata = {
+  title: "Perfil | Umov",
+  description: "Esta pgina es sobre tu perfil",
+  // other metadata
+};
+
+const Profile = () => {
+  return (
+    <>
+    <h1>Profile</h1>
+    </>
+  )
+}
+
+export default Profile;
